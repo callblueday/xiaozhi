72eb481a6cff2a3933f3c63fe790ae3580234494
callblueday <hujinhong@makeblock.com>
2017-04-08 10:41:16 +0800

    update sensor value getting

--- www/js/makeblockhd/sensors.js
+++ www/js/makeblockhd/sensors.js
@@ -3,20 +3,41 @@ MBlockly.Sensors = {
   sensorList : {
     'ULTRASONIC': 3,
     'LINEFOLLOW': 2,
-    'LIGHTSENSOR': 6
+    'LIGHTSENSOR': 6,
+    // 'GYROSCOPE': 0
   },
+  gyroValue: {
+    x: 0,
+    y: 0,
+    z: 0
+  }
 };
 
 MBlockly.Sensors.openSensor = function() {
   var that = this;
   for(var i in this.sensorList) {
     setTimeout((function(item) {
-      this['checkSensor-' + i] = setInterval(function() {
-          MBlockly.Control.getSensorValue(item, that.sensorList[item], function(val){
-            $('.' + item).text(val);
-          });
-      }, 200);
+      if(i !== "GYROSCOPE") {
+        this['checkSensor-' + i] = setInterval(function() {
+            MBlockly.Control.getSensorValue(item, that.sensorList[item], function(val){
+              $('.' + item).text(val);
+            });
+        }, 200);
+      }
     })(i), 100);
+  }
+}
+
+MBlockly.Sensors.openGyro = function() {
+  for(var i in this.gyroValue) {
+    setInterval(function() {
+    var extra = "";
+    if(i == 'GYROSCOPE') {
+      extra = 2; // 'x'
+    }
+    MBlockly.Control.getSensorValue(item, that.sensorList[item], function(val){
+      $('.' + item).text(val);
+    }, 2);
   }
 }
 
